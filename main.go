0829e8c3160d605cf6fc24eefc7692f506758d46
Xanonymous <trusaidlin@gmail.com>
2020-12-21 01:13:20 +0800

    seperate to go and php command

--- main.go
+++ main.go
@@ -7,11 +7,24 @@ import (
 
 func main() {
 	var name string
-
-	flag.StringVar(&name, "name", "X Go Cli", "description")
-	flag.StringVar(&name, "n", "X Go Cli", "description")
-
 	flag.Parse()
+
+	goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+	goCmd.StringVar(&name, "name", "Golang", "descriptions")
+	phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+	phpCmd.StringVar(&name, "n", "PHP lang", "descriptions")
+
+	args := flag.Args()
+	if len(args) <= 0 {
+		return
+	}
+
+	switch args[0] {
+	case "go":
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		_ = phpCmd.Parse(args[1:])
+	}
 
 	log.Printf("name: %s", name)
 }
